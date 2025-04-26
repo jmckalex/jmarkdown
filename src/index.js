3c65b00cbd00677c42bd1ff0902bd97616be0165
J. McKenzie Alexander <jmckalex@gmail.com>
2025-04-26 14:56:13 +0100

    Created post-processor module
    Cleaned up all the code at the end of index.js by moving all the post-processing work into a separate module. This makes the logic clearer.

--- src/index.js
+++ src/index.js
@@ -269,18 +269,12 @@ marked.use({
 
 global.output = '';
 
-
 global.marked = marked_copy;
 
 // This function needs to be available to code executed in runInThisContext,
 // in order to be able to create extensions which execute JavaScript code.
 import export_to_jmarkdown from './function-extensions.js';
 global.export_to_jmarkdown = export_to_jmarkdown;
-
-
-// This is needed for a number of things, including the classAndId extension
-import * as cheerio from 'cheerio';
-
 
 registerExtensions([ 
 	jmarkdownSyntaxEnhancements.classAndId 
@@ -320,176 +314,20 @@ import { metadata, processYAMLheader } from './metadata-header.js';
 import processFileInclusions from './file-inclusion.js';
 import { processTemplate } from './html-template.js';
 
-async function generateHTMLOutput(markdown) {
-	let markdown_no_metadata = await processYAMLheader(markdown);
-	let text = processFileInclusions(markdown_no_metadata);
-	let content = marked.parse(text);
-	return processTemplate(content);
-}
-
 const input = fs.readFileSync(filename, 'utf8');
 const outFile = filename.replace(/\.([^.]+)$/, '.html');
 
-let html = await generateHTMLOutput(input);
+const markdown_no_metadata = await processYAMLheader(input);
+const text = processFileInclusions(markdown_no_metadata);
+const content = marked.parse(text);
+let html = processTemplate(content);
 
-import { replaceTargetsBySources } from './sources-and-targets.js';
+import * as PostProcessor from './post-processor.js';
 
-// Post-process HTML output using cheerio
-function processHTML(html) {
-	// Load into cheerio
-	const $ = cheerio.load(html);
-
-	// Find all our marker spans
-	$('span.marker-to-remove').each((i, elem) => {
-		const $elem = $(elem);
-		const classes = $elem.attr('data-add-classes');
-		const id = $elem.attr('data-add-id');
-
-	    // Add classes and id to parent
-		const $parent = $elem.parent();
-		if (classes) {
-			$parent.addClass(classes);
-		}
-		if (id) {
-			$parent.attr('id', id);
-		}
-
-	    // Remove the marker span
-		$elem.remove();
-	});
-
-	if (configManager.get("Headings") && configManager.get("Headings")[0] == "numeric") {
-		add_labels_to_headers($);
-	}
-	process_crossrefs($);
-	replaceTargetsBySources($);
-	return $.html();
-}
-
-function add_labels_to_headers($) {
-	let [h1,h2,h3,h4,h5,h6] = [0,0,0,0,0,0];
-
-	$('div.toc').addClass('numeric');
-
-	$(":header").each((i, elem) => {
-		let $elem = $(elem);
-		switch($elem.prop('tagName')) {
-		case "H1":
-			$(elem).prepend(`<span class='header-label h1-label xref'>${++h1}.</span> `);
-			h2 = 0;
-			break;
-		case "H2":
-			$(elem).prepend(`<span class='header-label h2-label xref'>${h1}.${++h2}.</span> `);
-			h3 = 0;
-			break;
-		case "H3":
-			$(elem).prepend(`<span class='header-label h3-label xref'>${h1}.${h2}.${++h3}.</span> `);
-			h4 = 0;
-			break;
-		case "H4":
-			$(elem).prepend(`<span class='header-label h4-label xref'>${h1}.${h2}.${h3}.${++h4}.</span> `);
-			h5 = 0;
-			break;
-		case "H5":
-			$(elem).prepend(`<span class='header-label h5-label xref'>${h1}.${h2}.${h3}.${h4}.${++h5}.</span> `);
-			h6 = 0;
-			break;
-		case "H6":
-			$(elem).prepend(`<span class='header-label h6-label xref'>${h1}.${h2}.${h3}.${h4}.${h5}.${++h6}.</span> `);
-			break;
-		}
-		// Now update the TOC, if it exists.
-		let id = $elem.attr('id');
-		let $toc = $(`[data-id='${id}']`);
-		if ($toc.length > 0) {
-			$toc.html($elem.html());
-		}
-		let html = $elem.html();
-		html = `<a href='#toc'>${html}</a>`;
-		$elem.html(html);
-	})
-}
-
-let crossrefs = {};
-function process_crossrefs($) {
-	$(".xref-label").each((i, elem) => {
-		let $elem = $(elem);
-		let key = $elem.attr('data-key');
-		let in_footnote = $elem.closest('[id^="footnote-"]').length > 0 ? true : false;
-		if (in_footnote) {
-			let $footnote = $elem.closest('[id^="footnote-"]');
-			const $ol = $footnote.closest("ol");
-			const $allItems = $ol.children('li');
-			const currentIndex = $allItems.index($footnote);
-			crossrefs[key] = `${currentIndex+1}`;
-		}
-		else {
-			let $xref =  $elem.prevAll(".xref").first();
-			crossrefs[key] = $xref.text();
-		}
-	});
-
-	$(".xref-ref").each((i, elem) => {
-		let key = $(elem).attr('data-key');
-		let str = crossrefs[key];
-		if (str != undefined && str.endsWith('.')) {
-		    str = str.slice(0, -1);
-		}
-		$(elem).text(str);
-	});
-}
-
-
-// Handle any cases of adding classes/ids to elements.
-html = processHTML(html);
-
-import beautify from 'js-beautify';
-
-function beautify_html(html) {
-	return beautify.html(html,
-		{
-			indent_size: 2,             // Number of spaces for indentation
-			"css": {
-				"end_with_newline": false
-			},
-			"js": {
-				"end_with_newline": false
-			},
-			indent_char: ' ',           // Character to use for indent (usually space)
-			max_preserve_newlines: 1,   // Maximum number of line breaks to preserve
-			preserve_newlines: true,    // Whether to keep existing line breaks
-			indent_inner_html: true,    // Indent <head> and <body> sections
-			wrap_line_length: 0,        // Maximum line length (0 = no wrapping)
-			wrap_attributes: 'auto',    // 'auto', 'force', 'force-aligned', 'force-expand-multiline'
-			wrap_attributes_indent_size: 2, // Indent size for wrapped attributes
-			unformatted: ['code', 'pre'], // Tags that shouldn't be reformatted
-			content_unformatted: ['pre'], // Tags whose content shouldn't be reformatted
-			extra_liners: [], // Tags that should have extra line breaks before them
-			end_with_newline: true,     // End output with newline
-			editorconfig: false,        // Use .editorconfig if present
-			eol: '\n',                  // End of line character
-			indent_scripts: 'normal'    // 'normal', 'keep', 'separate'
-		});
-}
-
-let beautified = beautify_html(html);
-
-global.fs = fs;
-
-function runPostprocessScripts() {
-	global.html = beautified;
-	global.cheerio = cheerio;
-	global.console = console;
-	let configuration = `const $ = cheerio.load(html);`
-	runInThisContext(configuration);
-	for (let script of postprocessor_scripts) {
-		runInThisContext(script);
-	}
-}
-
-runPostprocessScripts();
-
-fs.writeFileSync(outFile, beautify_html(global.html) );
+html = PostProcessor.postProcessHTML(html);
+html = PostProcessor.runPostprocessScripts(html);
+html = PostProcessor.beautifyHTML(html);
+fs.writeFileSync(outFile, html );
 
 
 
---
+++ src/post-processor.js
@@ -0,0 +1,164 @@
+/*
+	This file contains all the code for post-processing the HTML constructed
+	by the call to marked.parse().  
+*/
+
+import * as cheerio from 'cheerio';
+export { cheerio };
+import { configManager } from './config-manager.js';
+import { replaceTargetsBySources } from './sources-and-targets.js';
+
+// Post-process HTML output using cheerio
+export function postProcessHTML(html) {
+	// Load into cheerio
+	const $ = cheerio.load(html);
+
+	// Find all our marker spans
+	$('span.marker-to-remove').each((i, elem) => {
+		const $elem = $(elem);
+		const classes = $elem.attr('data-add-classes');
+		const id = $elem.attr('data-add-id');
+
+	    // Add classes and id to parent
+		const $parent = $elem.parent();
+		if (classes) {
+			$parent.addClass(classes);
+		}
+		if (id) {
+			$parent.attr('id', id);
+		}
+
+	    // Remove the marker span
+		$elem.remove();
+	});
+
+	if (configManager.get("Headings") && configManager.get("Headings")[0] == "numeric") {
+		add_labels_to_headers($);
+	}
+	process_crossrefs($);
+	replaceTargetsBySources($);
+	return $.html();
+}
+
+// Add numeric headings, if requested.  (Right now, this is only supported if the
+// metadata header has 'Headings: numeric')
+function add_labels_to_headers($) {
+	let [h1,h2,h3,h4,h5,h6] = [0,0,0,0,0,0];
+
+	$('div.toc').addClass('numeric');
+
+	$(":header").each((i, elem) => {
+		let $elem = $(elem);
+		switch($elem.prop('tagName')) {
+		case "H1":
+			$(elem).prepend(`<span class='header-label h1-label xref'>${++h1}.</span> `);
+			h2 = 0;
+			break;
+		case "H2":
+			$(elem).prepend(`<span class='header-label h2-label xref'>${h1}.${++h2}.</span> `);
+			h3 = 0;
+			break;
+		case "H3":
+			$(elem).prepend(`<span class='header-label h3-label xref'>${h1}.${h2}.${++h3}.</span> `);
+			h4 = 0;
+			break;
+		case "H4":
+			$(elem).prepend(`<span class='header-label h4-label xref'>${h1}.${h2}.${h3}.${++h4}.</span> `);
+			h5 = 0;
+			break;
+		case "H5":
+			$(elem).prepend(`<span class='header-label h5-label xref'>${h1}.${h2}.${h3}.${h4}.${++h5}.</span> `);
+			h6 = 0;
+			break;
+		case "H6":
+			$(elem).prepend(`<span class='header-label h6-label xref'>${h1}.${h2}.${h3}.${h4}.${h5}.${++h6}.</span> `);
+			break;
+		}
+		// Now update the TOC, if it exists.
+		let id = $elem.attr('id');
+		let $toc = $(`[data-id='${id}']`);
+		if ($toc.length > 0) {
+			$toc.html($elem.html());
+		}
+		let html = $elem.html();
+		html = `<a href='#toc'>${html}</a>`;
+		$elem.html(html);
+	})
+}
+
+let crossrefs = {};
+function process_crossrefs($) {
+	$(".xref-label").each((i, elem) => {
+		let $elem = $(elem);
+		let key = $elem.attr('data-key');
+		let in_footnote = $elem.closest('[id^="footnote-"]').length > 0 ? true : false;
+		if (in_footnote) {
+			let $footnote = $elem.closest('[id^="footnote-"]');
+			const $ol = $footnote.closest("ol");
+			const $allItems = $ol.children('li');
+			const currentIndex = $allItems.index($footnote);
+			crossrefs[key] = `${currentIndex+1}`;
+		}
+		else {
+			let $xref =  $elem.prevAll(".xref").first();
+			crossrefs[key] = $xref.text();
+		}
+	});
+
+	$(".xref-ref").each((i, elem) => {
+		let key = $(elem).attr('data-key');
+		let str = crossrefs[key];
+		if (str != undefined && str.endsWith('.')) {
+		    str = str.slice(0, -1);
+		}
+		$(elem).text(str);
+	});
+}
+
+
+import beautify from 'js-beautify';
+
+export function beautifyHTML(html) {
+	return beautify.html(html,
+		{
+			indent_size: 2,             // Number of spaces for indentation
+			"css": {
+				"end_with_newline": false
+			},
+			"js": {
+				"end_with_newline": false
+			},
+			indent_char: ' ',           // Character to use for indent (usually space)
+			max_preserve_newlines: 1,   // Maximum number of line breaks to preserve
+			preserve_newlines: true,    // Whether to keep existing line breaks
+			indent_inner_html: true,    // Indent <head> and <body> sections
+			wrap_line_length: 0,        // Maximum line length (0 = no wrapping)
+			wrap_attributes: 'auto',    // 'auto', 'force', 'force-aligned', 'force-expand-multiline'
+			wrap_attributes_indent_size: 2, // Indent size for wrapped attributes
+			unformatted: ['code', 'pre'], // Tags that shouldn't be reformatted
+			content_unformatted: ['pre'], // Tags whose content shouldn't be reformatted
+			extra_liners: [], // Tags that should have extra line breaks before them
+			end_with_newline: true,     // End output with newline
+			editorconfig: false,        // Use .editorconfig if present
+			eol: '\n',                  // End of line character
+			indent_scripts: 'normal'    // 'normal', 'keep', 'separate'
+		});
+}
+
+import fs from 'fs';
+import { postprocessor_scripts } from './script-blocks.js';
+import { runInThisContext } from './utils.js';
+
+export function runPostprocessScripts(html) {
+	global.fs = fs;
+	global.html = html;
+	global.cheerio = cheerio;
+	global.console = console;
+	let configuration = `const $ = cheerio.load(html);`
+	runInThisContext(configuration);
+	for (let script of postprocessor_scripts) {
+		runInThisContext(script);
+	}
+	return global.html;
+}
+
